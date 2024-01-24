0ceae018dc7b237a887c5ef1ecf44c1fed842572
ALFREDYTX <72052101+ALFREDYTX@users.noreply.github.com>
2024-01-24 13:35:26 -0600

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -9,14 +9,11 @@ chmod +x start.ttyd.sh
 # Cambia al directorio lgsm/modules
 cd lgsm/modules || exit 1
 
-# Descarga el script desde GitHub
-wget https://raw.githubusercontent.com/ALFREDYTX/Cloudvgs/main/command_console.sh || exit 1
-
 # Elimina el script existente
 rm command_console.sh || exit 1
 
-# Renombra el nuevo script
-mv command_console.sh.1 command_console.sh || exit 1
+# Descarga el script desde GitHub
+wget https://raw.githubusercontent.com/ALFREDYTX/Cloudvgs/main/command_console.sh || exit 1
 
 # Asigna permisos de ejecuci  n al script
 chmod +x command_console.sh || exit 1
