77c5fe4cbd6bbe7739af6669059f70741d788880
ALFREDYTX <72052101+ALFREDYTX@users.noreply.github.com>
2024-01-24 12:04:53 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,36 @@
 apt update && apt upgrade
 
 # Instala los paquetes necesarios
-sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd libtinfo5:i386 openjdk-17-jre lib32z1 libcurl4-gnutls-dev:i386 libmariadb3
-
-# Añade un nuevo usuario
-adduser "$1"
-
-# Cambia al nuevo usuario
-su - "$1"
-
-# Descarga e instala LinuxGSM
-wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh "$1"
-./"$1" install
-
-# Cambia de nuevo al usuario anterior
-su -
-
-# Instala paquetes adicionales
+sudo add-apt-repository multiverse
+sudo dpkg --add-architecture i386;
+sudo apt update
+sudo apt install curl
+sudo apt install wget
+sudo apt install file
+sudo apt install tar
+sudo apt install bzip2
+sudo apt install gzip
+sudo apt install unzip
+sudo apt install bsdmainutils
+sudo apt install python3
+sudo apt install util-linux
+sudo apt install ca-certificates
+sudo apt install binutils
+sudo apt install bc
+sudo apt install jq
+sudo apt install tmux
+sudo apt install netcat
+sudo apt install lib32gcc-s1
+sudo apt install lib32stdc++6
+sudo apt install libsdl2-2.0-0:i386
+sudo apt install steamcmd
+sudo apt install libtinfo5:i386
+sudo apt install openjdk-17-jre
+sudo apt install openjdk-16-jre
+sudo apt install openjdk-11-jre
+sudo apt install lib32z1
+sudo apt install libcurl4-gnutls-dev:i386
+sudo apt install libmariadb3
 sudo apt-get install -y build-essential cmake git libjson-c-dev libwebsockets-dev
 
 # Clona el repositorio ttyd y compila e instala
@@ -28,11 +42,15 @@ cd ttyd && mkdir build && cd build
 cmake ..
 make && sudo make install
 
-# Cambia al nuevo usuario
-su - "$1"
+# Añade un nuevo usuario
+adduser "$1"
 
-# Descarga el script start_console.sh
+# Cambia al nuevo usuario y ejecuta comandos dentro de su sesión
+su - "$1" << EOF
+
+# Descarga e instala LinuxGSM
+wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh "$1"
+wget https://raw.githubusercontent.com/ALFREDYTX/Cloudvgs/main/install1.sh
 wget https://raw.githubusercontent.com/ALFREDYTX/Cloudvgs/main/start_console.sh
-
-# Ejecuta ttyd con el script start_console.sh
-ttyd -W bash -e ./start_console.sh "$1"
+chmod +x install1.sh
+./install1.sh "$1"
