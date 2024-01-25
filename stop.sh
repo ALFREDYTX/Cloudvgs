7f88d940166a0f623e55996e53823f39ae17b147
ALFREDYTX <72052101+ALFREDYTX@users.noreply.github.com>
2024-01-25 10:32:10 -0600

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,2 +1,10 @@
-username=$USER
-screen -S $username -X stuff '^C'
+#!/bin/bash
+
+# Obtener los PID de las sesiones de screen
+pids=$(pgrep -l screen | awk '{print $1}')
+
+# Iterar sobre los PID y enviar la señal de terminación
+for pid in $pids; do
+    echo "Matando el proceso screen con PID $pid"
+    kill -TERM $pid
+done
