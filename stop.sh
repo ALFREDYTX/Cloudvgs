cc874fff2a91131c93a9d5b784891798b6cc5b26
ALFREDYTX <72052101+ALFREDYTX@users.noreply.github.com>
2024-01-25 10:39:25 -0600

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-# Obtener los PID de las sesiones de screen
-pids=$(pgrep -l screen | awk '{print $1}')
-
-# Iterar sobre los PID y enviar la señal de terminación
-for pid in $pids; do
-    echo "Matando el proceso screen con PID $pid"
-    kill -TERM $pid
-done
+su - minecraft <<EOF
+screen -S minecraft-server -X stuff "stop$(printf \\r)"
+EOF
