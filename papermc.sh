f4ffbdc16e9e993de2c04d051960b418f1be13da
ALFREDYTX <72052101+ALFREDYTX@users.noreply.github.com>
2024-01-25 10:59:37 -0600

    Update papermc.sh

--- papermc.sh
+++ papermc.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Actualizar e instalar dependencias
 apt --assume-yes update && apt --assume-yes upgrade
 apt install --assume-yes screen
 apt install --assume-yes ttyd  
@@ -7,13 +10,14 @@ apt install --assume-yes openjdk-11-jre-headless
 apt install --assume-yes openjdk-8-jre-headless
 apt install --assume-yes jq
 apt install --assume-yes curl
-curl https://raw.githubusercontent.com/ALFREDYTX/Cloudvgs/main/start.sh --output /start.sh
-curl https://raw.githubusercontent.com/ALFREDYTX/Cloudvgs/main/stop.sh --output /stop.sh
-chmod +x /start.sh
-chmod +x /stop.sh
+curl -s https://raw.githubusercontent.com/ALFREDYTX/Cloudvgs/main/stop.sh --output /stop.sh
+curl -s https://raw.githubusercontent.com/ALFREDYTX/Cloudvgs/main/start.sh --output /start.sh
 
 # Solicitar el nombre del usuario
 read -p "Ingrese el nombre del usuario: " username
+
+# Solicitar el nombre del usuario
+read -p "Ingrese la cantidad de ram maxima: " ram
 
 # Agregar usuario interactivo
 sudo useradd $username
@@ -25,8 +29,30 @@ sudo mkdir /home/$username
 sudo chown $username:$username /home/$username
 
 # Establecer la contraseña de manera no interactiva
-echo "$username:12345" | sudo chpasswd
+echo "$username:123456" | sudo chpasswd
 
+# Establecer el directorio de inicio de usuario
+STOP_SCRIPT="/stop.sh"
+START_SCRIPT="/start.sh"
+
+# Verificar si los archivos existen
+if [ ! -f "$STOP_SCRIPT" ] || [ ! -f "$START_SCRIPT" ]; then
+    echo "Error: Los archivos stop.sh o start.sh no existen en las rutas proporcionadas."
+    exit 1
+fi
+
+# Reemplazar "name-server" con el valor de $username y "user" con el valor de $username en stop.sh
+sed -i "s/name-server/$username/g" "$STOP_SCRIPT"
+sed -i "s/user/$username/g" "$STOP_SCRIPT"
+
+# Reemplazar "name-server" con el valor de $username, "command-start" con el valor de "java -Xms512M -Xmx${ram} -jar server.jar nogui" y "user" con el valor de $username en start.sh
+sed -i "s/name-server/$username/g" "$START_SCRIPT"
+sed -i "s/user/$username/g" "$START_SCRIPT"
+sed -i "s/command-start/java -Xms512M -Xmx${ram} -jar server.jar nogui/g" ""
+
+echo "Los archivos stop.sh y start.sh han sido modificados exitosamente."
+
+# Solicitar la versión de Minecraft
 read -p "Ingrese la versión de Minecraft: " version
 
 PROJECT=paper
@@ -71,5 +97,34 @@ else
     echo "Error: Download failed. Please check your internet connection."
     exit 1
 fi
+
+SERVICE_FILE="/etc/systemd/system/ttyd.service"
+
+# Verifica si el usuario actual tiene permisos de escritura en el archivo de servicio
+if [ ! -w "$SERVICE_FILE" ]; then
+    echo "No tienes permisos para escribir en $SERVICE_FILE. Ejecuta el script con privilegios de superusuario."
+    exit 1
+fi
+
+# Contenido del archivo de servicio
+SERVICE_CONTENT="[Unit]
+Description=$username Server
+
+[Service]
+Type=simple
+User=$username
+WorkingDirectory=/home/$username
+ExecStart=/usr/bin/ttyd -W -p 8080 /usr/bin/screen -x $username
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"
+
+# Escribe el contenido en el archivo de servicio
+echo "$SERVICE_CONTENT" | sudo tee "$SERVICE_FILE" > /dev/null
+
+# Notifica al usuario que la operación se completó
+echo "El archivo de servicio ha sido creado con éxito en $SERVICE_FILE."
 
 su - $username
